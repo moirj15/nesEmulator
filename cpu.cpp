9e87be54faa169b68aa8816ceb6838581c2cff1e
moirj15 <moirj15@gmail.com>
2018-01-30 22:02:11 -0500

    started work on add carry instructin

--- cpu.cpp
+++ cpu.cpp
@@ -1,5 +1,17 @@
 #include "cpu.h"
 
+// Add with carry instructions
+#define ADC_IMM		0x69
+#define ADC_ZP		0x65
+#define ADC_ZPX		0x75
+#define ADC_ABS		0x6D
+#define ADC_ABS_X	0x7D
+#define ADC_ABS_Y	0x79
+#define ADC_IND_X	0x61
+#define ADC_IND_Y	0x71
+
+#define NOP		0xEA
+
 Cpu::Cpu(void)
 {
 	A = 0;
@@ -7,7 +19,7 @@ Cpu::Cpu(void)
 	Y = 0;
 	//TODO: set SP and PC
 	status = 0;
-	remaningCycles = CYCLES_PER_FRAME;
+	remainingCycles = CYCLES_PER_FRAME;
 }
 
 Cpu::~Cpu(void)
@@ -24,4 +36,134 @@ void Cpu::tick(void)
 	// NOTE: the cpu runs at 1.79 MHz which comes down to roughly 29834 cycles
 	// per frame
 	remainingCycles--;
+}
+
+void Cpu::run(void)
+{
+	switch (memory[PC]) {
+		case NOP:
+			nop();
+			break;
+	}
+}
+
+/**
+ * Add with carry instructions.
+ */
+void Cpu::addc_imm(void)
+{
+	pc++;
+	u8 preAdd = A >> 7;
+	A += memory[pc] + (CARRY & status);
+	u8 postAdd = A >> 7;
+	status |= (preAdd != postAdd);
+	status |= (A == 0) << 1;
+	status |= (A & NEGATIVE) << 7;
+	pc++;
+	tick();
+	tick();
+}
+
+void Cpu::addc_zp(void)
+{
+	addc_imm();
+	tick();
+}
+
+void Cpu::addc_zpx(void)
+{
+	pc++;
+	u8 preAdd = A >> 7;
+	A += memory[memory[pc] + X] + (CARRY & status);
+	u8 postAdd = A >> 7;
+	status |= (preAdd != postAdd);
+	status |= (A == 0) << 1;
+	status |= (A & NEGATIVE) << 7;
+	pc++;
+
+	tick();
+	tick();
+	tick();
+	tick();
+
+}
+
+void Cpu::addc_abs(void)
+{
+	pc++;
+	u8 preAdd = A >> 7;
+	u16 memLoc = memory[pc] << 8;
+	pc++;
+	memLoc |= memory[pc];
+	A += memory[memLoc] + (CARRY & status);
+	u8 postAdd = A >> 7;
+	status |= (preAdd != postAdd);
+	status |= (A == 0) << 1;
+	status |= (A & NEGATIVE) << 7;
+	pc++;
+	tick();
+	tick();
+	tick();
+	tick();
+}
+
+void Cpu::addc_abs_x(void)
+{
+	pc++;
+	u8 preAdd = A >> 7;
+	u16 memLoc = memory[pc] << 8;
+	pc++;
+	memLoc |= memory[pc];
+	A += memory[memLoc + X] + (CARRY & status);
+	u8 postAdd = A >> 7;
+	status |= (preAdd != postAdd);
+	status |= (A == 0) << 1;
+	status |= (A & NEGATIVE) << 7;
+	pc++;
+	tick();
+	tick();
+	tick();
+	tick();
+	//TODO: check if page is crossed
+
+}
+
+void Cpu::addc_abs_y(void)
+{
+	pc++;
+	u8 preAdd = A >> 7;
+	u16 memLoc = memory[pc] << 8;
+	pc++;
+	memLoc |= memory[pc];
+	A += memory[memLoc + Y] + (CARRY & status);
+	u8 postAdd = A >> 7;
+	status |= (preAdd != postAdd);
+	status |= (A == 0) << 1;
+	status |= (A & NEGATIVE) << 7;
+	pc++;
+	tick();
+	tick();
+	tick();
+	tick();
+	//TODO: check if page is crossed
+}
+
+void Cpu::addc_ind_x(void)
+{
+
+}
+
+void Cpu::addc_ind_y(void)
+{
+
+}
+	
+/**
+ * No operation.
+ */
+void Cpu::nop(void)
+{
+	pc++;
+	tick();
+	tick();
 }
--- cpu.h
+++ cpu.h
@@ -3,7 +3,17 @@
 
 #include "utils.h"
 
-struct Cpu
+struct flags 
+{
+	CARRY = 1,
+	ZERO = 2,
+	INTERRUPT_DISSABLE = 4,
+	DECIMAL_MODE = 8,
+	OVERFLOW = 64,
+	NEGATIVE = 128,
+};
+
+class Cpu
 {
 	// General purpose registers
 	u8 	A;
@@ -20,13 +30,35 @@ struct Cpu
 	const s32 CYCLES_PER_FRAME = 29834;
 	s32 remainingCycles; //borrowed from github/AndreaOrru/LaiNES 
 
+public:
 	Cpu(void);
 	~Cpu(void);
 
+	void run(void);
+	
+private:
 	/**
  	 * Run the Cpu for a cycle.
  	 */
 	void tick(void);
+
+	/**
+	 * Add with carry instructions.
+	 */
+	void addc_imm(void);
+	void addc_zp(void);
+	void addc_zpx(void);
+	void addc_abs(void);
+	void addc_abs_x(void);
+	void addc_abs_y(void);
+	void addc_ind_x(void);
+	void addc_ind_y(void);
+
+	/**
+	 * No operation.
+	 */
+	void nop(void);
+
 };
 
 
