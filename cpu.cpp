102bf7c37417353e88135e173a92fac2b226916f
moirj15 <moirj15@gmail.com>
2018-02-07 16:17:08 -0500

    almost done with first instruction

--- cpu.cpp
+++ cpu.cpp
@@ -38,9 +38,124 @@ void Cpu::tick(void)
 	remainingCycles--;
 }
 
+
+// memory access methods
+// All memory access methods grab the address from the opCode's operands.
+// In otherwords, once an opcode is read the memory methods will increment
+// the program counter and then use the new value in the program counter
+// to get the value that will be used for accessing memory.
+
+//TODO: write better method descriptions.
+
+/**
+ * Grabs the immediate value from the next byte in program memory.
+ *
+ * @return: The value found at the next byte.
+ */
+u8 Cpu::getImmediate(void)
+{
+	pc++;
+	u8 ret = memory[pc];
+	pc++;
+	return ret;
+}
+
+
+/**
+ * Grabs the next byte from program memory and uses that to retrieve a 
+ * value from the zero page.
+ *
+ * @return: The value found in the zero page.
+ */
+u8 Cpu::getZeroPage(void)
+{
+	return getImmediate();
+}
+
+/**
+ * Grabs the next byte from program memory and adds that to the value found
+ * in the X register and uses that new value to retrieve a value from the
+ * zero page.
+ *
+ * @return: The value found in the zero page.
+ */
+u8 Cpu::getZeroPageIndexed(void)
+{
+	return memory[getImmediate() + X];
+}
+
+/**
+ * Grabs the 16 bit address from the next two bytes and uses that to access
+ * a value in memory.
+ *
+ * @return: The value found at the absolute address.
+ */
+u8 Cpu::getAbsolute(void)
+{
+	pc++;
+	u16 ret = memory[pc] << 8;
+	pc++;
+	ret |= memory[pc];
+	pc++;
+
+	return ret;
+}
+
+/**
+ * Grabs the 16 bit address from the next two bytes and adds it to the 
+ * value found in the X register and uses the calculated value to fetch a
+ * value from memory.
+ *
+ * @return: The value found in memory.
+ */
+u8 Cpu::getAbsoluteXIndex(void)
+{
+	return memory[getAbsolute() + X];
+}
+
+/**
+ * Grabs the 16 bit address from the next two bytes and adds it to the 
+ * value found in the y register and uses the calculated value to fetch a
+ * value from memory.
+ *
+ * @return: The value found in memory.
+ */
+u8 Cpu::getAbsoluteYIndex(void)
+{
+	return memory[getAbsolute() + Y];
+}
+
+/**
+ * The value in the X register is added to the next byte. This result is
+ * used to grab a 16 bit value from the zero page which is then used to
+ * access a value in memory.
+ *
+ * @return: The value found in memory.
+ */
+u8 Cpu::getIndexedIndirect(void)
+{
+	u8 zp = getImmediate() + X;
+	u16 ret = memory[zp] << 8;
+	ret |= memory[zp + 1];
+	return memory[ret];
+}
+
+/**
+ * Takes the value in the next byte, uses that to retrieve a 16 bit value
+ * from the zero page and adds the value in the Y register to that. The 
+ * result is then used to access a value in memory.
+ *
+ * @return: The value found in memory.
+ */
+u8 Cpu::getIndirectIndexed(void)
+{
+
+}
+
+
 void Cpu::run(void)
 {
-	switch (memory[PC]) {
+	switch (memory[pc]) {
 		case NOP:
 			nop();
 			break;
@@ -131,15 +246,9 @@ void Cpu::addc_abs_x(void)
 void Cpu::addc_abs_y(void)
 {
 	pc++;
-	u8 preAdd = A >> 7;
 	u16 memLoc = memory[pc] << 8;
-	pc++;
-	memLoc |= memory[pc];
-	A += memory[memLoc + Y] + (CARRY & status);
-	u8 postAdd = A >> 7;
-	status |= (preAdd != postAdd);
-	status |= (A == 0) << 1;
-	status |= (A & NEGATIVE) << 7;
+	memLoc |= memory[pc + 1];
+	A += memory[memLoc] + (CARRY & status);
 	pc++;
 	tick();
 	tick();
@@ -150,13 +259,35 @@ void Cpu::addc_abs_y(void)
 
 void Cpu::addc_ind_x(void)
 {
-
+	pc++;
+	u16 memLoc = memory[memory[pc]] + X;
+	memLoc |= memory[memory[pc]] + X + 1;
+	A += memory[memLoc] + (CARRY & status);
+	tick();
+	tick();
+	tick();
+	tick();
+	tick();
+	tick();
+	//TODO: check if page is crossed
 }
 
 void Cpu::addc_ind_y(void)
 {
-
+	pc++;
+	u16 memLoc = memory[memory[pc]] << 8;
+	memLoc |= memory[memory[pc] + 1];
+	memLoc += Y;
+	A += memory[memLoc] + (CARRY & status);
+	pc++;
+	tick();
+	tick();
+	tick();
+	tick();
+	tick();
+	//TODO: check if page is crossed
 }
+
 	
 /**
  * No operation.
--- cpu.h
+++ cpu.h
@@ -3,7 +3,7 @@
 
 #include "utils.h"
 
-struct flags 
+enum FLAGS 
 {
 	CARRY = 1,
 	ZERO = 2,
@@ -21,9 +21,9 @@ class Cpu
 	u8 	Y;
 
 	// Special purpose registers
-	u8 	SP;
+	u8 	sp;
 	u8 	status;
-	u16 PC;
+	u16 pc;
 
 	u8 memory[0x10000]; // for now allocate the entire address space for the emulator
 
@@ -41,6 +41,82 @@ private:
  	 * Run the Cpu for a cycle.
  	 */
 	void tick(void);
+
+	// memory access methods
+	// All memory access methods grab the address from the opCode's operands.
+	// In otherwords, once an opcode is read the memory methods will increment
+	// the program counter and then use the new value in the program counter
+	// to get the value that will be used for accessing memory.
+
+	//TODO: write better method descriptions.
+
+	/**
+	 * Grabs the immediate value from the next byte in program memory.
+	 *
+	 * @return: The value found at the next byte.
+	 */
+	u8 getImmediate(void);
+
+	/**
+	 * Grabs the next byte from program memory and uses that to retrieve a 
+	 * value from the zero page.
+	 *
+	 * @return: The value found in the zero page.
+	 */
+	u8 getZeroPage(void);
+
+	/**
+	 * Grabs the next byte from program memory and adds that to the value found
+	 * in the X register and uses that new value to retrieve a value from the
+	 * zero page.
+	 *
+	 * @return: The value found in the zero page.
+	 */
+	u8 getZeroPageIndexed(void);
+
+	/**
+	 * Grabs the 16 bit address from the next two bytes and uses that to access
+	 * a value in memory.
+	 *
+	 * @return: The value found at the absolute address.
+	 */
+	u8 getAbsolute(void);
+
+	/**
+	 * Grabs the 16 bit address from the next two bytes and adds it to the 
+	 * value found in the X register and uses the calculated value to fetch a
+	 * value from memory.
+	 *
+	 * @return: The value found in memory.
+	 */
+	u8 getAbsoluteXIndex(void);
+
+	/**
+	 * Grabs the 16 bit address from the next two bytes and adds it to the 
+	 * value found in the y register and uses the calculated value to fetch a
+	 * value from memory.
+	 *
+	 * @return: The value found in memory.
+	 */
+	u8 getAbsoluteYIndex(void);
+
+	/**
+	 * The value in the X register is added to the next byte. This result is
+	 * used to grab a 16 bit value from the zero page which is then used to
+	 * access a value in memory.
+	 *
+	 * @return: The value found in memory.
+	 */
+	u8 getIndexedIndirect(void);
+
+	/**
+	 * Takes the value in the next byte, uses that to retrieve a 16 bit value
+	 * from the zero page and adds the value in the Y register to that. The 
+	 * result is then used to access a value in memory.
+	 *
+	 * @return: The value found in memory.
+	 */
+	u8 getIndirectIndexed(void);
 
 	/**
 	 * Add with carry instructions.
