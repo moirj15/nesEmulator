74cd3d4ad9cfff281c2e5b5f31ac2cd74bd8425c
moirj15 <moirj15@gmail.com>
2019-01-08 19:36:08 -0500

    working on opcodes

--- instructions.h
+++ instructions.h
@@ -4,13 +4,18 @@
 #include "utils.h"
 
 enum ADDRESS_MODES : u8{
+    IMPLIED,
     IMMEDIATE,
     ZERO_PAGE,
     ZERO_PAGE_X,
     ABSOLUTE,
+    ABSOLUTE_X,
     ABSOLUTE_Y,
+    INDIRECT,
     INDEXED_INDIRECT, // (Indirect, X)
     INDIRECT_INDEXED, // (Indirect), Y
+    ACCUMULATOR,
+    RELATIVE
 };
 
 struct OpCode {
